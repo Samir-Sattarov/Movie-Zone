6c8a66b9e020c23bb57ce3d82790ba721882ebc4
Samir-Sattarov <samirsattarov2004@gmail.com>
2023-12-13 22:27:18 +0500

    initialize

---
+++ lib/application.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:movie_zone/features/main/presentation/screens/home_screen.dart';
+
+import 'main.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+
+  }
+}
---
+++ lib/locator/locator.dart
@@ -0,0 +1,15 @@
+import 'package:get_it/get_it.dart';
+
+final locator = GetIt.I;
+
+void setup() {
+  // ================ BLoC / Cubit ================ //
+
+  // ================ UseCases ================ //
+
+  // ================ Repository / Datasource ================ //
+
+  // ================ Core ================ //
+
+  // ================ External ================ //
+}
